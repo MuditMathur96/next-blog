537354f81fad9a75bae673d46047ad7f81789417
Mudit Mathur <mudit.manu.mathur@gmail.com>
2024-01-08 16:16:45 +0530

    authentication complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+"use client"
+import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 
+import { authOptions } from './api/auth/[...nextauth]/route'
+
 export default function Home() {
+
+  const {data,status} = useSession()
   return (
-    <main>
-      <h1>Next App</h1>
+    <main className='p-2'>
+      <h1>{status}</h1>
+      <h1>{JSON.stringify(data)}</h1>
+      <Image 
+      src={data?.user?.image || ""}
+      alt=""
+      height={100}
+      width={100}
+      />
     </main>
   )
 }
